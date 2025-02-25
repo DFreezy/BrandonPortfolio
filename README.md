f19701bde361eb4c0c4a7ca3663a87a227a574f2
Du-Wayne Frieslaar <Du-Wayne.Frieslaar@capaciti.org.za>
2025-02-25 15:36:54 +0200

    added new background

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+https://www.figma.com/design/6jV0Th4o3DzXCxcZwQ5Psy/Untitled?node-id=0-1&p=f&t=6DuF8KNTLEg8ukJZ-0
+
+https://brandonseptember.netlify.app/#
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,11 +4,30 @@ import {Link} from 'react-router-dom'
 export default function() {
     return (
         <>
-        <button>Resume</button>
-        <button>Experience</button>
-        <button>Certifications</button>
-        <button>Education</button>
-        <button>Contact</button>
+       <div className="flex flex-wrap justify-center gap-4 py-4 bg-gray-900 text-white">
+        <button className="text-2xl hover:scale-110 transition-transform">☀</button>
+        <button className="p-2 text-2xl rounded-full bg-gray-800 text-white hover:bg-gray-600 hover:scale-110 transition-transform duration-200">
+  <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+  <img src="/menu-hamburger-svgrepo-com.svg" alt="icon" className="w-6 h-6" />
+  </svg>
+</button>
+
+        <button className="px-6 py-2 bg-gray-700 rounded-lg shadow-md hover:bg-gray-500 transition">
+          Resume
+        </button>
+      <button className="px-6 py-2 bg-gray-700 rounded-lg shadow-md hover:bg-gray-500 transition">
+        Experience
+      </button>
+      <button className="px-6 py-2 bg-gray-700 rounded-lg shadow-md hover:bg-gray-500 transition">
+        Certifications
+      </button>
+      <button className="px-6 py-2 bg-gray-700 rounded-lg shadow-md hover:bg-gray-500 transition">
+        Education
+      </button>
+      <button className="px-6 py-2 bg-gray-700 rounded-lg shadow-md hover:bg-gray-500 transition">
+        Contact
+      </button>
+    </div>
         </>
     )
 }
@@ -16,10 +35,3 @@ export default function() {
 
 
 
-
-{/*Home
-Resume
-Experience
-Certifications
-Education
-Contact*/} 
